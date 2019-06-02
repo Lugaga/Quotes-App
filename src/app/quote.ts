2960f39d8711fa05ab49fe60301c6ace5bfcda40
Lugaga <maureezgaga@gmail.com>
2019-06-02 19:26:59 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,5 +0,0 @@
-export class Quote {
-  constructor(public id:number,public publisher:string,public author:string,public quote:string,
-    public upvotes:number,public downvotes:number,public time:number,public highest:boolean){
-  }
-}
