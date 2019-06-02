d6818eb7bff79bf800963a53d004d45cadccb599
Lugaga <maureezgaga@gmail.com>
2019-06-02 16:31:26 +0300

    add the form component

---
+++ src/app/form-component/form-component.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
+@Component({
+  selector: 'app-form-component',
+  templateUrl: './form-component.component.html',
+  styleUrls: ['./form-component.component.css']
+})
+export class FormComponentComponent implements OnInit {
+public quote:Quote=new Quote(0,' ',' ',' ',0,0,0,false);
+@Output() isComplete=new EventEmitter<Quote>();
+  constructor(){
+  }
+  sendsOutput(){
+    if(!(this.quote.quote==' ') && !(this.quote.author== ' ') && !(this.quote.publisher==' ')){
+        this.isComplete.emit(this.quote);
+    }
+    else{
+      alert("Sorry ,All fields are required!")
+    }
+    this.quote=new Quote(0,' ',' ',' ',0,0,0,false);
+  }
+  ngOnInit() {
+  }
+
+}
